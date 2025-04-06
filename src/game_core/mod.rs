6ed0e9e4cac73edb5794c11325bfdf721ba8f1c6
Cemonix <gittlermilan@seznam.cz>
2025-04-06 21:09:31 +0200

    Add game_core module to organize game components and resources

---
+++ src/game_core/mod.rs
@@ -0,0 +1,11 @@
+pub use resources::ResourceType;
+pub use building::{Building, BuildingType};
+pub use planet::Planet;
+pub use player::Player;
+pub use game::Game;
+
+pub mod resources;
+pub mod building;
+pub mod planet;
+pub mod player;
+pub mod game;
