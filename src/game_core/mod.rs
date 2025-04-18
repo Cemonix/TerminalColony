a02712f350885f9ae7c160fc2c6be675e07ada99
Cemonix <gittlermilan@seznam.cz>
2025-04-18 10:28:36 +0200

    Refactor mod.rs to reorganize module imports and ensure proper visibility of game components

--- src/game_core/mod.rs
+++ src/game_core/mod.rs
@@ -1,11 +1,13 @@
-pub use resources::ResourceType;
-pub use building::{Building, BuildingType};
-pub use planet::Planet;
-pub use player::Player;
-pub use game::Game;
-
+pub mod command_handler;
 pub mod resources;
 pub mod building;
 pub mod planet;
 pub mod player;
-pub mod game;
+pub mod game_core;
+
+pub use command_handler::CommandHandler;
+pub use resources::ResourceType;
+pub use building::{Building, BuildingBase, BuildingDefinitionId, BuildingError, BuildingBehavior, ResourceGenerator, StorageProvider};
+pub use planet::Planet;
+pub use player::Player;
+pub use game_core::GameCore;
