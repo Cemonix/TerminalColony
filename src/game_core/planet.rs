39f793920bcf919cf16f4c518ef89d86b586f259
Cemonix <gittlermilan@seznam.cz>
2025-04-20 17:20:34 +0200

    Refactor Planet module: Enhance error handling with PlanetError enum, update building management, and streamline resource checks

--- src/game_core/planet.rs
+++ src/game_core/planet.rs
@@ -1,50 +1,130 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
+use std::error::Error;
 
-use super::building::{Building, BuildingType};
+use super::building::building::Building;
+use super::building::{BuildingError, BuildingType, BuildingTypeId};
+use super::resource::Resource;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum PlanetError {
+    BuildingNotBuilt,
+    InsufficientResources,
+    BuildingError(BuildingError),
+}
+
+impl fmt::Display for PlanetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PlanetError::BuildingNotBuilt => write!(f, "Building not built"),
+            PlanetError::InsufficientResources => write!(f, "Insufficient resources"),
+            PlanetError::BuildingError(err) => write!(f, "Building error: {}", err),
+        }
+    }
+}
+
+impl Error for PlanetError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            PlanetError::BuildingNotBuilt => None,
+            PlanetError::InsufficientResources => None,
+            PlanetError::BuildingError(err) => Some(err),
+        }
+    }
+}
+
+impl From<BuildingError> for PlanetError {
+    fn from(err: BuildingError) -> Self {
+        PlanetError::BuildingError(err)
+    }
+}
+
+// =================================================================================================
 
 pub struct Planet {
     name: String,
-    buildings: Vec<Building>,
-    building_slots: HashMap<BuildingType, u8>,
-    size: u32,
-    category: String,
+    buildings: HashMap<BuildingTypeId, BuildingType>,
 }
 
 impl Planet {
-    // pub fn new(name: String, buildings: Vec<Building>, size: u32, category: String) -> Self {
-    //     let mut building_slots = HashMap::new();
-    //     for building in &buildings {
-    //         *building_slots.entry(building.building_type).or_insert(0) += 1;
-    //     }
-    //     Planet {
-    //         name,
-    //         buildings,
-    //         size,
-    //         category,
-    //     }
-    // }
+    pub fn new(name: String, buildings: Option<HashMap<BuildingTypeId, BuildingType>>) -> Self {
+        Self {
+            name,
+            buildings: buildings.unwrap_or_else(HashMap::new),
+        }
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn build(
+        &mut self,
+        building: BuildingType,
+        energy: u32,
+        minerals: u32,
+        gas: u32,
+    ) -> Result<(), PlanetError> {
+        self.has_enough_resources(energy, minerals, gas)?;
+
+        if let Some(existing_building) = self.buildings.get_mut(&building.get_id()) {
+            existing_building.upgrade()?;
+        } else {
+            self.buildings.insert(building.get_id(), building);
+        }
+
+        Ok(())
+    }
+
+    fn get_resource_storage(
+        &self,
+        resource: Resource,
+    ) -> Result<BuildingType, PlanetError> {
+        match resource {
+            Resource::Energy => {
+                if let Some(building) = self.buildings.get(&BuildingTypeId::BatteryArray) {
+                    return Ok(building.clone());
+                }
+                Err(PlanetError::BuildingNotBuilt)
+            }
+            Resource::Minerals => {
+                if let Some(building) = self.buildings.get(&BuildingTypeId::MineralStorage) {
+                    return Ok(building.clone());
+                }
+                Err(PlanetError::BuildingNotBuilt)
+            }
+            Resource::Gas => {
+                if let Some(building) = self.buildings.get(&BuildingTypeId::GasTank) {
+                    return Ok(building.clone());
+                }
+                Err(PlanetError::BuildingNotBuilt)
+            }
+        }
+    }
+    
+    fn has_enough_resources(
+        &self,
+        energy: u32,
+        minerals: u32,
+        gas: u32,
+    ) -> Result<(), PlanetError> {
+        match self.get_resource_storage(Resource::Energy)? {
+            BuildingType::BatteryArray(ref storage) if energy <= storage.get_current_amount() => {}
+            BuildingType::BatteryArray(_) => return Err(PlanetError::InsufficientResources),
+            _ => return Err(PlanetError::BuildingNotBuilt),
+        }
+
+        match self.get_resource_storage(Resource::Minerals)? {
+            BuildingType::MineralStorage(ref storage) if minerals <= storage.get_current_amount() => {}
+            BuildingType::MineralStorage(_) => return Err(PlanetError::InsufficientResources),
+            _ => return Err(PlanetError::BuildingNotBuilt),
+        }
+
+        match self.get_resource_storage(Resource::Gas)? {
+            BuildingType::GasTank(ref storage) if gas <= storage.get_current_amount() => {}
+            BuildingType::GasTank(_) => return Err(PlanetError::InsufficientResources),
+            _ => return Err(PlanetError::BuildingNotBuilt),
+        }
+
+        Ok(())
+    }
 }
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_planet_creation() {
-//         let planet = Planet::new(
-//             "Earth".to_string(), 
-//             vec![
-//                 Building::new(BuildingType::CommandCenter, 1, 5),
-//                 Building::new(BuildingType::FusionReactor, 1, 5),
-//             ], 
-//             100, 
-//             "Terrestrial".to_string()
-//         );
-
-//         assert_eq!(planet.name, "Earth");
-//         assert_eq!(planet.buildings.len(), 2);
-//         assert_eq!(planet.size, 100);
-//         assert_eq!(planet.category, "Terrestrial");
-//     }
-// }
