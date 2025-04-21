a67d1dd7e2785cc5be898d6bca00357cb77d1fca
Cemonix <gittlermilan@seznam.cz>
2025-04-21 11:02:28 +0200

    Add Turn struct: Implement turn management with methods for creating, advancing, and resetting turns

---
+++ src/game_core/turn.rs
@@ -0,0 +1,21 @@
+pub struct Turn {
+    turn_number: u32,
+}
+
+impl Turn {
+    pub fn new(turn_number: u32) -> Self {
+        Turn { turn_number }
+    }
+    
+    pub fn get_turn_number(&self) -> u32 {
+        self.turn_number
+    }
+
+    pub fn next_turn(&mut self) {
+        self.turn_number += 1;
+    }
+
+    pub fn reset_turn(&mut self) {
+        self.turn_number = 0;
+    }
+}
