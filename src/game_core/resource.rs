f0c3c049aa4a02aba6bda2d61d15caf7272a817e
Cemonix <gittlermilan@seznam.cz>
2025-04-23 20:55:48 +0200

    Enhance Resource enum: Add missing import for fmt and ensure proper implementation of Display trait for improved string representation

--- src/game_core/resource.rs
+++ src/game_core/resource.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -6,4 +8,14 @@ pub enum Resource {
     Minerals,
     Gas,
     Energy,
+}
+
+impl fmt::Display for Resource {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Resource::Minerals => write!(f, "Minerals"),
+            Resource::Gas => write!(f, "Gas"),
+            Resource::Energy => write!(f, "Energy"),
+        }
+    }
 }
