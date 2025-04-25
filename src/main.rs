d84d37a0dbb3405328dcb0d47e7914d9e41dbe6c
Cemonix <gittlermilan@seznam.cz>
2025-04-25 12:29:51 +0200

    Add TODO comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let _ = App::new().unwrap().run();
 }
 
-// TODO: Implement command execution.
+// TODO: Building a building does not consume resources
+// TODO: Figure out how to handle building time
 // TODO: Change help command for question mark which will show help for all commands
 // TODO: Change quit command for exiting the app and ask for confirmation
 // TODO: Main menu - new game, load game, settings
